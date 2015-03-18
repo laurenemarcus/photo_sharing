01545fda2a1a45ec8d21a9b104cf42deef92a001
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-18 14:36:59 -0700

    Track user sessions, allow users to log in and out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-  @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome"
       redirect_to "/"
