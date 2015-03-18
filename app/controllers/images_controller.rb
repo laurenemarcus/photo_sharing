d38e9ededf764cb7732162353dd184ba56c226df
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-18 16:35:54 -0700

    Add images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,17 @@
 class ImagesController < ActiveRecord::Base
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+def create
+  @image = Image.create(image_params)
+end
+# know id of the user and then attach the image to the user. we do not need to create a new user.
+
+private
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+def user_params
+  params.require(:user).permit(:images)
+end
+
 end
