6c604cde06036ca1d9b285cca089f65b68464d2e
Arsi <argoncode18@gmail.com>
2015-03-19 10:17:08 -0700

    Upload and display images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,19 +1,24 @@
-class ImagesController < ActiveRecord::Base
+class ImagesController < ApplicationController
+  def index
+  end
 
   def create
     @image = Image.create(image_params)
+    redirect_to new_image_path
   end
   # know id of the user and then attach the image to the user. we do not need to create a new user.
 
   def new
     @image = Image.new
+    @images = Image.all
   end
 
   # Use strong_parameters for attribute whitelisting
   # Be sure to update your create() and update() controller methods.
+private
 
-  def user_params
-    params.require(:user).permit(:images)
+  def image_params
+    params.require(:image).permit(:image)
   end
 
 end
